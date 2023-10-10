3eefad927508f0249708b78f24c6893035d2d94d
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-10 10:44:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-let char = '';
-	let i=0;
-	while(i<str.length()-1){
-		char = str.charAt(i);
-		var s = str.subString(i+1,str.length+1);
-		if(!s.contains(char){
-			return char;
-		}
-		return null;
-	}
-}
-// const input = prompt("Enter a string");
-// alert(firstNonRepeatedChar(input)); 
+ const charCount = new Map();
+
+  // Count the occurrences of each character in the string
+  for (let char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  // Find the first non-repeated character
+  for (let char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input)); 
