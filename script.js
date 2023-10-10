615ed836e3f37f15396bee467a7ace5f1bcced05
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-10 10:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+let char = '';
+	let i=0;
+	while(i<str.length()-1){
+		char = str.charAt(i);
+		var s = str.subString(i+1,str.length+1);
+		if(!s.contains(char)){
+			return char;
+		}
+		return null;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
